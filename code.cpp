7640143b0a39c5441ed84b9b583a4614b4e94991
Ahsan Ali <ahsanali19112009@gmail.com>
2024-10-14 03:06:16 +0500

    code is completed but it's messy rn

--- code.cpp
+++ code.cpp
@@ -17,24 +17,107 @@ std::cout << "How Many Rounds Do You Want To Play : " ;
 std::cin >> roundCount ;
 std::cout << "--------------------------------------- \n" ;
 
-
+int userScore ;
+int botScore ;
 
 for (int i = 0 ; roundCount > i ; i++) {
+
+
 
 std::cout << "1. Rock \n" ;
 std::cout << "2. Paper \n" ;
 std::cout << "3. Scissors \n" ;
 
-    int userMove ;
+    int userMove  ;
     std::cout << "Chose Your Move : " ;
     std::cin >> userMove ;
 
 srand ( time ( NULL ) ) ;
 int botMove = ( rand() % 3 ) + 1 ;
+// std::cout << botMove << std::endl ;
 
+// this code under is for rock = rock,paper,scissors.
 
+if ( userMove == 1 && botMove == 2){
 
+    std::cout << "Bot Wins This Round \n" ;
+    botScore++ ;
+
+}
+else if (userMove == 1 && botMove == 3) {
+
+    std::cout << "You Won This Round \n" ;
+    userScore++ ;
+
+}
+else if (userMove == 1 && botMove == 1) {
+
+std::cout << "Both Got The Same, Tie ! \n" ;
+
+}
+
+// this code under is for Paper = papers,rock,scissors.
+
+if (userMove == 2 && botMove == 1) {
+
+std::cout << "You Won This Round \n" ;
+userScore++;
     }
+
+else if ( userMove == 2 && botMove == 3 ) {
+
+std::cout << "Bot Wins This Round \n" ;
+botScore++ ;
+
+}
+
+else if ( userMove == 2 && botMove ==2 ) {
+
+std::cout << "Bot Got The Same, Tie ! \n" ;
+
+}
+
+// this code under is for Scissors  = scissors,paper,rock.
+
+if(userMove == 3 && botMove == 1){
+
+std::cout << "Bot Wins This Round \n" ;
+botScore++ ;
+
+}
+
+else if (userMove == 3 && botMove == 2) {
+
+std::cout << "You Won This Round \n" ;
+userScore ++ ;
+
+}
+
+else if (userMove == 3 && botMove == 3) {
+
+std::cout << "Both Got The Same, Tie ! \n" ;
+
+}
 
 
 }
+
+if (userScore > botScore) {
+
+std::cout << "YOU WON THE GAME !!! \n" ;
+
+}
+
+else if (botScore > userScore) {
+
+std::cout << "YOU LOST !!! \n" ;
+
+}
+
+else if (userScore == botScore){
+
+std::cout << "IT'S A TIE ! \n" ;
+
+}
+
+    }
