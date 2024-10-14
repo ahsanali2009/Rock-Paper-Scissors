071b9559198f36b0ff7ba7c4cd6928700f03359c
Ahsan Ali <ahsanali19112009@gmail.com>
2024-10-14 05:06:21 +0500

    final commit

--- code.cpp
+++ code.cpp
@@ -23,103 +23,107 @@ srand (time(NULL)) ;
 for (int i = 0 ; roundCount > i ; i++) {
 
 
-
 std::cout << "1. Rock \n" ;
 std::cout << "2. Paper \n" ;
 std::cout << "3. Scissors \n" ;
+
 
     int userMove  ;
     std::cout << "Chose Your Move : " ;
     std::cin >> userMove ;
 
-int botMove = ( rand() % 3 ) + 1 ;
-// std::cout << botMove << std::endl ;
+    int botMove = ( rand() % 3 ) + 1 ;
+
 
 // this code under is for rock = rock,paper,scissors.
 
-if ( userMove == 1 && botMove == 2){
+if ( userMove == 1 && botMove == 2 ) { // rock and paper
 
     std::cout << "Bot Wins This Round : " << botMove << std::endl << std::endl ;
     botScore++ ;
 
 }
-else if (userMove == 1 && botMove == 3) {
 
-    std::cout << "You Won This Round \n \n" ;
-    userScore++ ;
+     else if ( userMove == 1 && botMove == 3 ) { //rock and scissors
+
+        std::cout << "You Won This Round \n \n" ;
+        userScore++ ;
 
 }
-else if (userMove == 1 && botMove == 1) {
 
-std::cout << "Both Got The Same, Tie ! \n \n" ;
+        else if ( userMove == 1 && botMove == 1 ) { //rock and rock
+
+             std::cout << "Both Got The Same, Tie ! \n \n" ;
 
 }
 
 // this code under is for Paper = papers,rock,scissors.
 
-if (userMove == 2 && botMove == 1) {
+if ( userMove == 2 && botMove == 1 ) { //paper and rock
 
-std::cout << "You Won This Round \n \n" ;
-userScore++;
+    std::cout << "You Won This Round \n \n" ;
+    userScore++;
+
     }
 
-else if ( userMove == 2 && botMove == 3 ) {
+        else if ( userMove == 2 && botMove == 3 ) { //paper and scissor
 
-std::cout << "Bot Wins This Round : "<< botMove << std::endl << std::endl ;
-botScore++ ;
+            std::cout << "Bot Wins This Round : "<< botMove << std::endl << std::endl ;
+            botScore++ ;
 
 }
 
-else if ( userMove == 2 && botMove ==2 ) {
+            else if ( userMove == 2 && botMove ==2 ) { //paper and paper
 
-std::cout << "Both Got The Same, Tie ! \n \n" ;
+                std::cout << "Both Got The Same, Tie ! \n \n" ;
 
 }
 
 // this code under is for Scissors  = scissors,paper,rock.
 
-if(userMove == 3 && botMove == 1){
+if ( userMove == 3 && botMove == 1 ) { //scissors and rock
 
-std::cout << "Bot Wins This Round : " << botMove << std::endl << std::endl;
-botScore++ ;
-
-}
-
-else if (userMove == 3 && botMove == 2) {
-
-std::cout << "You Won This Round \n \n" ;
-userScore ++ ;
+    std::cout << "Bot Wins This Round : " << botMove << std::endl << std::endl;
+    botScore++ ;
 
 }
 
-else if (userMove == 3 && botMove == 3) {
+    else if ( userMove == 3 && botMove == 2 ) { //scissors and paper
 
-std::cout << "Both Got The Same, Tie ! \n \n" ;
+        std::cout << "You Won This Round \n \n" ;
+        userScore ++ ;
+
+}
+
+        else if (userMove == 3 && botMove == 3) { //paper and paper
+
+            std::cout << "Both Got The Same, Tie ! \n \n" ;
 
 }
 
-
-}
+    }   
 
 botScore =  botScore - 8;
 
-if (userScore > botScore) {
+if ( userScore > botScore ) {
 
 std::cout << " \n \nYOU WON THE GAME !!! \n" ;
 std::cout << "You : " << userScore << " Bot : " << botScore ;
 
 }
 
-else if (userScore < botScore) {
+else if ( userScore < botScore ) {
 
 std::cout << "\n \nYOU LOST !!! \n" ;
 std::cout << "You : " << userScore << " | " << "Bot : " << botScore ;
+
 }
 
-else if (userScore == botScore){
+else if ( userScore == botScore ){
 
 std::cout << "\nIT'S A TIE ! \n" ;
 
 }
+
 
     }
